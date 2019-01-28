346a71fb3ddaae0e8c261d9694a2068f3285672c
webpropopuli <webpropopuli@gmail.com>
2019-01-28 11:43:06 -0500

    Added Express and server.js. Now posts reneder at client and server re-writes to proper names

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 import Router from "next/router";
 import Layout from "../components/Layout.js";
 
-/* we'll pass in data for a postroll */
+/* we'll pass in data for a postroll using route masking (<Link as=...)*/
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -19,7 +19,10 @@ export default () => (
       <Link href="/about">
         <button style={{ fontSize: 20 }}> all about</button>
       </Link>
-      <Postlink title=" some silly thing"> text here</Postlink>
+      <ul>
+        <PostLink id="postHello" title="Hello Next.js" />
+        <PostLink id="postLearn" title="Learn Next.js is awesome" />
+      </ul>
     </p>
     Click <a onClick={() => Router.push("/about")}>here</a> to read more
   </Layout>
